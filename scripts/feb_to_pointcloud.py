0af057627554985742ba67737bf1bf25635f7778
stormf <femke.storm@sioux.eu>
2026-03-10 17:01:40 +0100

    Fixed: problems with removing back wall

--- scripts/feb_to_pointcloud.py
+++ scripts/feb_to_pointcloud.py
@@ -1,25 +1,15 @@
-################################################################################################
-# The following script should be opened within the Blender python API. That is the 'scripting' #
-# section in the top ribbon. Make sure that the paths are set up correctly, by changing the    #
-# input for "filepath".                                                                        #
-################################################################################################
 
 from pathlib import Path
-
-import numpy as np
-
-#  Assign the path to the single .feb file you wish to load in.
-filepath = Path(r"C:\Users\stormf\PycharmProjects\EWS-FEM-pipeline\all_static_settings.feb")
-
 import logging
-from pathlib import Path
 
 import numpy as np
 import pyvista as pv
 from tqdm import tqdm
 
-logger = logging.getLogger(__name__)
+#  Assign the path to the single .feb file you wish to load in.
+filepath = Path(r"C:\Users\stormf\PycharmProjects\EWS-FEM-pipeline\all_static_settings_HGO_no_tumor.feb")
 
+logger = logging.getLogger(__name__)
 
 assert filepath.suffix == ".feb", "The input file does not have the correct file extension. Must be .feb"
 
@@ -34,21 +24,22 @@ filepath_vtk_0 = filepath_name.with_suffix(f".0.vtk")
 # Get mesh data from unperturbed breast
 vtk_mesh = pv.read(filepath_vtk_0)
 vtk_points = vtk_mesh.points  # Vertices
+vtk_mesh['orig_indices'] = np.arange(vtk_mesh.n_points, dtype = np.int32)
 
 # Find all displacements from vtk file
 displacement = vtk_mesh.active_vectors  # Get displacement
 
 # Let displacements work on initial configuration
-vtk_mesh_disp = vtk_mesh
-vtk_mesh_disp.points = vtk_points + displacement
+vtk_mesh_disp = vtk_mesh.copy()
+vtk_mesh_disp.points = vtk_mesh_disp.points + displacement
 
 # Extract only surface
-surfs = vtk_mesh_disp.extract_surface(algorithm='dataset_surface')
-surf_ids = surfs['vtkOriginalPointIds']
-chest_ids = np.where(displacement[:,0] == 0)
-front_ids = np.setdiff1d(surf_ids, chest_ids)
+surfs = vtk_mesh.extract_surface(algorithm='dataset_surface', pass_pointid = True)
+surf_ids = surfs['orig_indices']
 
-#Now contains both skin and glandular surfaces. Extract only skin surface
-skin_mesh = surfs.connectivity(extraction_mode='largest')
-skin_points = skin_mesh.points
+# Remove chest wall
+chest_ids = np.where(displacement[:,0] == 0)
+chest = vtk_points[chest_ids]
+front = np.setdiff1d(surf_ids, chest_ids[0])
+
 
