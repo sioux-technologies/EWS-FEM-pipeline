a0948421185d013ac6be52f376d14ee3630df51c
stormf <femke.storm@sioux.eu>
2026-03-11 10:37:18 +0100

    Added: file to convert feb output to a point cloud

--- scripts/feb_to_pointcloud.py
+++ scripts/feb_to_pointcloud.py
@@ -10,36 +10,37 @@ from tqdm import tqdm
 filepath = Path(r"C:\Users\stormf\PycharmProjects\EWS-FEM-pipeline\all_static_settings_HGO_no_tumor.feb")
 
 logger = logging.getLogger(__name__)
+def feb_to_pointcloud(filepath: Path):
+    assert filepath.suffix == ".feb", "The input file does not have the correct file extension. Must be .feb"
 
-assert filepath.suffix == ".feb", "The input file does not have the correct file extension. Must be .feb"
+    # Get name of input file
+    filename = filepath.stem
 
-# Get name of input file
-filename = filepath.stem
+    # Output directory
+    filepath_output = filepath.parent / "output"
+    filepath_name = filepath_output / filename
+    filepath_vtk_0 = filepath_name.with_suffix(f".0.vtk")
 
-# Output directory
-filepath_output = filepath.parent / "output"
-filepath_name = filepath_output / filename
-filepath_vtk_0 = filepath_name.with_suffix(f".0.vtk")
+    # Get mesh data from unperturbed breast
+    vtk_mesh = pv.read(filepath_vtk_0)
+    vtk_points = vtk_mesh.points  # Vertices
+    vtk_mesh['orig_indices'] = np.arange(vtk_mesh.n_points, dtype = np.int32)
 
-# Get mesh data from unperturbed breast
-vtk_mesh = pv.read(filepath_vtk_0)
-vtk_points = vtk_mesh.points  # Vertices
-vtk_mesh['orig_indices'] = np.arange(vtk_mesh.n_points, dtype = np.int32)
+    # Find all displacements from vtk file
+    displacement = vtk_mesh.active_vectors  # Get displacement
 
-# Find all displacements from vtk file
-displacement = vtk_mesh.active_vectors  # Get displacement
+    # Let displacements work on initial configuration
+    vtk_mesh_disp = vtk_mesh.copy()
+    vtk_mesh_disp.points = vtk_mesh_disp.points + displacement
 
-# Let displacements work on initial configuration
-vtk_mesh_disp = vtk_mesh.copy()
-vtk_mesh_disp.points = vtk_mesh_disp.points + displacement
+    # Extract only surface
+    surfs = vtk_mesh.extract_surface(algorithm='dataset_surface', pass_pointid = True)
+    surf_ids = surfs['orig_indices']
 
-# Extract only surface
-surfs = vtk_mesh.extract_surface(algorithm='dataset_surface', pass_pointid = True)
-surf_ids = surfs['orig_indices']
-
-# Remove chest wall
-chest_ids = np.where(displacement[:,0] == 0)
-chest = vtk_points[chest_ids]
-front = np.setdiff1d(surf_ids, chest_ids[0])
+    # Remove chest wall
+    chest_ids = np.where(displacement[:,0] == 0)
+    front = np.setdiff1d(surf_ids, chest_ids[0])
+    skin = pv.PolyData(vtk_mesh_disp.points[front])
+    skin.save(filepath_name.with_suffix(f".ply"))
 
 
