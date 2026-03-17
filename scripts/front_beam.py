c8f29c44a5431df024d09c86123062a8156a111d
stormf <femke.storm@sioux.eu>
2026-03-17 10:16:22 +0100

    Added working breast segmentation

--- scripts/front_beam.py
+++ scripts/front_beam.py
@@ -3,6 +3,20 @@ from pathlib import Path
 import numpy as np
 import pyvista as pv
 from load_data import load_obj_file, point_clicker
+from ews_fem_pipeline.prepare_simulation import Settings
+
+
+def extract_breast(skin: pv.PolyData):
+    more_points = np.array(point_clicker(skin))
+    more_points[:, 1] = 0
+    breast_circumf = pv.Spline(more_points, closed=True)
+    breast_area = breast_circumf.delaunay_2d()
+    breast_area.translate((0, 0.5, 0), inplace=True)
+    breast_volume = breast_area.extrude((0, -1, 0), capping=True)
+    skin_segmented = skin.select_interior_points(breast_volume, inside_out=True)
+    skin_segmented = skin_segmented.threshold(0.5)
+    skin_segmented = skin_segmented.extract_surface(algorithm=None)
+    return skin_segmented
 
 # filepath = Path(r"C:\Users\stormf\PycharmProjects\EWS-FEM-pipeline\output\all_static_settings_HGO_no_tumor.obj")
 filepath = Path(r"C:\Users\stormf\OneDrive - Sioux Group B.V\Documents\EWS data\EWS_dataset\3031.01.lr.frame_001.obj")
@@ -12,24 +26,14 @@ nipple_coord = point_clicker(skin)
 #translate such that the nipple is at the origin
 skin_pd = pv.PolyData(skin.points)
 skin_pd.translate(-1*nipple_coord[0], inplace=True)
-skin.points = skin_pd.points
+skin.points = skin_pd.points*0.25
 
-more_points = np.array(point_clicker(skin))
-more_points[:,1] = 0
-breast_circumf = pv.Spline(more_points, closed=True)
-breast_area = breast_circumf.delaunay_2d()
-breast_area.translate((0, 0.5, 0), inplace=True)
-breast_volume = breast_area.extrude((0,-1,0), capping=True)
-skin_segmented = skin.select_interior_points(breast_volume)
-
-
-
-
+skin_segmented = extract_breast(skin)
 
 dist_grid = 0.01
 bounds = np.array(skin_segmented.bounds)
 m = 15
-n = 20
+n = int(np.floor(np.max(np.abs(bounds))/dist_grid))
 lines = []
 inters = []
 for theta in np.linspace(0,2*np.pi, m, endpoint=False):
