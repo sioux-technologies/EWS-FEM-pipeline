9236de2183554167c2d7420959d9f1eff5c7d56b
stormf <femke.storm@sioux.eu>
2026-03-18 11:13:43 +0100

    Added custom message to clicker interface

--- scripts/front_beam.py
+++ scripts/front_beam.py
@@ -8,8 +8,8 @@ from ews_fem_pipeline.prepare_simulation import generate_mesh, write_to_feb
 from ews_fem_pipeline.cli import generate, fem
 from ews_fem_pipeline.convert_simulation import feb_to_3d
 
-def extract_breast(skin: pv.PolyData):
-    more_points = np.array(point_clicker(skin))
+def extract_breast(skin: pv.PolyData | pv.UnstructuredGrid):
+    more_points = np.array(point_clicker(skin, message = 'Click points around breast area'))
     more_points[:, 1] = 0
     breast_circumf = pv.Spline(more_points, closed=True)
     breast_area = breast_circumf.delaunay_2d()
@@ -23,7 +23,7 @@ def extract_breast(skin: pv.PolyData):
 # filepath = Path(r"C:\Users\stormf\PycharmProjects\EWS-FEM-pipeline\output\all_static_settings_HGO_no_tumor.obj")
 filepath = Path(r"C:\Users\stormf\OneDrive - Sioux Group B.V\Documents\EWS data\EWS_dataset\3031.01.lr.frame_001.obj")
 skin = load_obj_file(filepath)
-nipple_coord = point_clicker(skin)
+nipple_coord = point_clicker(skin, message='Click point for nipple')
 
 #translate such that the nipple is at the origin
 skin_pd = pv.PolyData(skin.points)
@@ -47,8 +47,6 @@ for theta in np.linspace(0,2*np.pi, m, endpoint=False):
             inters.append(projection_point)
         else:
             inters.append([np.nan, np.nan, np.nan])
-points_pd = pv.PolyData(inters)
-# pv.plot(points_pd)
 
 settings=Settings()
 settings.model.geometry.radius = float(np.abs((bounds[2]-bounds[3])))
