b36c084a4402e65368d96f1c88b61f6848921deb
stormf <femke.storm@sioux.eu>
2026-03-25 15:51:34 +0100

    Improved object alignment

--- scripts/front_beam.py
+++ scripts/front_beam.py
@@ -1,16 +1,12 @@
 import copy
 from pathlib import Path
-
 import numpy as np
 import pyvista as pv
 from load_data import load_obj_file, point_clicker
 from ews_fem_pipeline.prepare_simulation import Settings, write_settings_to_toml
-from ews_fem_pipeline.prepare_simulation import generate_mesh, write_to_feb
 from ews_fem_pipeline.cli import generate, fem
 from ews_fem_pipeline.convert_simulation.feb_to_3d import feb_to_3d
-
 import open3d as o3d
-
 
 def extract_breast(skin: pv.PolyData | pv.UnstructuredGrid):
     more_points = np.array(point_clicker(skin, message='Click points around breast area '))
@@ -24,14 +20,14 @@ def extract_breast(skin: pv.PolyData | pv.UnstructuredGrid):
     skin_segmented = skin_segmented.extract_surface(algorithm=None)
     return skin_segmented
 
-
-def projection_points(skin_segmented: pv.PolyData, m=15, dist_points=0.01):
+def generate_projection_points(skin_segmented: pv.PolyData, dist_points=0.01):
     intercepts = []
     points = np.empty((0, 2))
     bounds = np.array(skin_segmented.bounds)
     n = int(np.floor(np.max(np.abs(bounds)) / dist_points))
-    for theta in np.linspace(0, 2 * np.pi, m, endpoint=False):
-        for ring in range(n):
+    for ring in range(n):
+        m = int(np.floor(2*np.pi*ring))
+        for theta in np.linspace(0, 2 * np.pi, m, endpoint=False):
             x = ring * dist_points * np.sin(theta)
             z = ring * dist_points * np.cos(theta)
             projection_point = skin_segmented.ray_trace([x, 1, z], [x, -1, z], first_point=True)[0]
@@ -39,7 +35,6 @@ def projection_points(skin_segmented: pv.PolyData, m=15, dist_points=0.01):
                 intercepts.append(projection_point)
                 points = np.append(points, np.array([x, z]).reshape(1, -1), axis=0)
     return points, intercepts
-
 
 def project_front(surface: pv.PolyData | Path, points: np.ndarray):
     intercepts = []
@@ -50,7 +45,6 @@ def project_front(surface: pv.PolyData | Path, points: np.ndarray):
         else:
             intercepts.append([np.nan, np.nan, np.nan])
     return intercepts
-
 
 def match_settings(skin: pv.PolyData):
     bounds = np.array(skin.bounds)
@@ -63,52 +57,58 @@ def match_settings(skin: pv.PolyData):
 
 if __name__ == "__main__":
     # Import target surface and determine center (nipple)
-    filepath = Path(r"C:\Users\stormf\OneDrive - Sioux Group B.V\Documents\EWS data\EWS_dataset\3032_01_lr.frame_001.obj")
-    skin = load_obj_file(filepath)
-    nipple_coord = point_clicker(skin, message='Click point for nipple ')
+    filepath = Path(r"C:\Users\stormf\OneDrive - Sioux Group B.V\Documents\EWS data\EWS_dataset\3034_01_lr.frame_001.obj")
+    skin = load_obj_file(filepath, scale = 0.2) #data is not to scale, hence the 0.2 (guesstimated)
 
     # Translate such that the nipple is at the origin
-    skin_pd = pv.PolyData(skin.points)
-    skin_pd.translate(-1*nipple_coord[0], inplace=True)
-    skin.points = skin_pd.points*0.25
+    nipple_coord = point_clicker(skin, message='Click point for nipple. ')
+    skin.translate(-1*nipple_coord[0], inplace=True)
 
     # Segment the breast and get projection points
     skin_segmented = extract_breast(skin)
-    points, projected_real = projection_points(skin_segmented)
-    settings = match_settings(skin_segmented)
+    points, projected_real = generate_projection_points(skin_segmented)
 
     # Prepare settings and output files
     folder = Path(r"C:\Users\stormf\PycharmProjects\EWS-FEM-pipeline\optimization")
     filepath_out = Path(folder) / filepath.stem
     filepath_out_toml = filepath_out.with_suffix(f'.toml')
-    write_settings_to_toml(filepath = filepath_out_toml, settings = settings)
 
-    # Generate model
+    # first guess for radius and write settings
+    first_guess_settings = match_settings(skin)
+    write_settings_to_toml(filepath = filepath_out_toml, settings = match_settings(skin_segmented))
+
+    # Generate mesh, run, and generate displaced mesh .obj file
     mesh_files = generate.callback([filepath_out_toml])
     feb_files = fem.callback(mesh_files, jobs=0)
     obj_files = feb_to_3d(feb_files[0])
 
+    # Load resulting mesh
     surface = load_obj_file(obj_files, switch_axes=False)
+    # Translate to have nipple at (0,0,0)
     surface.translate(-1*surface.points[np.argmax(surface.points[:,1])], inplace=True)
+    #Project projection points on model mesh
     surface= surface.extract_surface(algorithm=None)
     projected_model = project_front(surface, points)
 
-
+    #Load both clouds into open3d
     pcd_model = o3d.geometry.PointCloud()
     pcd_model.points = o3d.utility.Vector3dVector(np.array(projected_model))
     pcd_target = o3d.geometry.PointCloud()
     pcd_target.points = o3d.utility.Vector3dVector(np.array(projected_real))
-    evaluation = o3d.pipelines.registration.evaluate_registration(
-        pcd_target, pcd_model, 1)
-    pcd_model_trans = copy.deepcopy(pcd_model)
-    print(evaluation)
 
-    reg_p2p = o3d.pipelines.registration.registration_icp(pcd_target, pcd_model, 1, estimation_method=o3d.pipelines.registration.TransformationEstimationPointToPoint())
-    pcd_model_trans.transform(1*reg_p2p.transformation)
-    evaluation = o3d.pipelines.registration.evaluate_registration(
-        pcd_target, pcd_model_trans, 1)
-    print(evaluation)
 
-    pcd_model_trans.paint_uniform_color([1, 0.706, 0])
-    pcd_target.paint_uniform_color([0, 0.651, 0.929])
-    o3d.visualization.draw_geometries([pcd_target, pcd_model_trans])
+    correspondence = o3d.utility.Vector2iVector(np.repeat(np.where(np.sum(projected_real, axis=1) != np.nan), 2).reshape(-1, 2))
+
+
+    estimator = o3d.pipelines.registration.TransformationEstimationPointToPoint()
+    print(estimator.compute_rmse(pcd_target, pcd_model, correspondence))
+    transformation = estimator.compute_transformation(pcd_model, pcd_target, correspondence)
+    # Copy model cloud and transform
+    pcd_model_trans = copy.deepcopy(pcd_model).transform(transformation)
+    print(estimator.compute_rmse(pcd_target, pcd_model_trans, correspondence))
+
+
+    pcd_model_trans.paint_uniform_color([1, 0.706, 0]) #yellow
+    pcd_target.paint_uniform_color([0, 0.651, 0.929]) #blue
+    o3d.visualization.draw_geometries([pcd_target, pcd_model_trans, pcd_model])
+
